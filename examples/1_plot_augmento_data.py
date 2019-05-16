9afc3b927cfcb53218a17b35ea3f63a620ee968e
Oliver Spragg <oliver@converge.io>
2019-05-16 18:48:27 +0200

    cleaned up plot_augmento_data

--- examples/1_plot_augmento_data.py
+++ examples/1_plot_augmento_data.py
@@ -8,41 +8,28 @@ import matplotlib.dates as md
 
 # import files from src
 sys.path.insert(0, "src")
-import helper_functions as hf
+import example_helper as eh
 
 # define the location of the input file
 filename_augmento_topics = "data/example_data/augmento_topics.msgpack.zlib"
 filename_augmento_data = "data/example_data/augmento_data.msgpack.zlib"
 filename_bitmex_data = "data/example_data/bitmex_data.msgpack.zlib"
 
-# load the topics
-with open(filename_augmento_topics, "rb") as f:
-	temp = hf.decode_bytes(msgpack.unpackb(zlib.decompress(f.read())))
-	augmento_topics = {int(k) : v for k, v in temp.items()}
-	augmento_topics_inv = {v : int(k) for k, v in temp.items()}
-
-# load the augmento data
-with open(filename_augmento_data, "rb") as f:
-	temp = hf.decode_bytes(msgpack.unpackb(zlib.decompress(f.read())))
-	t_augmento_data = np.array([el["t_epoch"] for el in temp], dtype=np.float64)
-	augmento_data = np.array([el["counts"] for el in temp], dtype=np.int32)
-
-# load the price data
-with open(filename_bitmex_data, "rb") as f:
-	temp = hf.decode_bytes(msgpack.unpackb(zlib.decompress(f.read())))
-	t_price_data = np.array([el["t_epoch"] for el in temp], dtype=np.float64)
-	price_data = np.array([el["close"] for el in temp], dtype=np.float64)
+all_data = eh.load_example_data(filename_augmento_topics,
+                             filename_augmento_data,
+                             filename_bitmex_data)
+aug_topics, aug_topics_inv, t_aug_data, aug_data, t_price_data, price_data = all_data
 
 # get the signals we're interested in
-augmento_signal_a = augmento_data[:, augmento_topics_inv["Positive"]]
-augmento_signal_b = augmento_data[:, augmento_topics_inv["Negative"]]
+aug_signal_a = aug_data[:, aug_topics_inv["Positive"]]
+aug_signal_b = aug_data[:, aug_topics_inv["Negative"]]
 
 # plot some data!
 fig, ax = plt.subplots(2, 1, sharex=True, sharey=False)
 
 # initialise some labels for the plot
-datenum_augmento_data = [md.date2num(datetime.datetime.fromtimestamp(el))
-							for el in t_augmento_data]
+datenum_aug_data = [md.date2num(datetime.datetime.fromtimestamp(el))
+							for el in t_aug_data]
 datenum_price_data = [md.date2num(datetime.datetime.fromtimestamp(el))
 							for el in t_price_data]
 
@@ -50,8 +37,8 @@ datenum_price_data = [md.date2num(datetime.datetime.fromtimestamp(el))
 ax[0].grid(linewidth=0.4)
 ax[1].grid(linewidth=0.4)
 ax[0].plot(datenum_price_data, price_data, linewidth=0.5)
-ax[1].plot(datenum_augmento_data, augmento_signal_a, color="g", linewidth=0.5)
-ax[1].plot(datenum_augmento_data, augmento_signal_b, color="r", linewidth=0.5)
+ax[1].plot(datenum_aug_data, aug_signal_a, color="g", linewidth=0.5)
+ax[1].plot(datenum_aug_data, aug_signal_b, color="r", linewidth=0.5)
 
 # label axes
 ax[0].set_ylabel("Price")
