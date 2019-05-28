e0544da8e3f022d484ce7bc23d96b2ab439128ea
Oliver Spragg <oliver@converge.io>
2019-05-28 10:09:00 +0200

    moved datetime_end for data loaders to 2019-04-01

--- examples/2_load_bitmex_example_data.py
+++ examples/2_load_bitmex_example_data.py
@@ -20,7 +20,7 @@ filename_save_data = "{:s}/bitmex_data.msgpack.zlib".format(path_save_data)
 
 # define the start and end times
 datetime_start = datetime.datetime(2017, 1, 1)
-datetime_end = datetime.datetime(2019, 1, 1)
+datetime_end = datetime.datetime(2019, 4, 1)
 
 # initialise a store for the data we're downloading
 market_data = []
