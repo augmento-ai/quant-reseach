9bc320a2c5425768c4719ae438e015e73ec04804
Oliver Spragg <oliver@converge.io>
2019-05-28 14:49:45 +0200

    updated and commented market_event_example_1

--- examples/5_market_event_example_1.py
+++ examples/5_market_event_example_1.py
@@ -6,6 +6,11 @@ import datetime
 import matplotlib.pyplot as plt
 import matplotlib.dates as md
 
+"""
+This script plots the 'twitter/bitcoin/Fork' Augmento signal against the XBt price between the 10th of October 2018, and the 3rd of December 2018, using Twitter as a source.
+The number of 'Fork' events clearly increase before the Bitcoin Cash hard fork on the 15th ofNovember 2018.
+"""
+
 # import files from src
 sys.path.insert(0, "src")
 import example_helper as eh
@@ -17,58 +22,57 @@ filename_augmento_data = "data/example_data/augmento_data.msgpack.zlib"
 filename_bitmex_data = "data/example_data/bitmex_data.msgpack.zlib"
 
 # load the example data
+datetime_start = datetime.datetime(2018, 10, 8)
+datetime_end = datetime.datetime(2018, 12, 3)
 all_data = eh.load_example_data(filename_augmento_topics,
                              filename_augmento_data,
                              filename_bitmex_data,
-                             datetime_start=datetime.datetime(2018, 8, 29),
-                             datetime_end=datetime.datetime(2018, 9, 10))
+                             datetime_start=datetime_start,
+                             datetime_end=datetime_end)
 aug_topics, aug_topics_inv, t_aug_data, aug_data, t_price_data, price_data = all_data
 
-print(aug_topics_inv.keys())
+# initialise the smooth data
+aug_data_smooth = aug_data.astype(np.float64)
 
-# get the signals we're interested in
-#aug_signal_a = aug_data[:, aug_topics_inv["Bullish"]].astype(np.float64)
-#aug_signal_b = aug_data[:, aug_topics_inv["Bearish"]].astype(np.float64)
-#aug_signal_a = aug_data[:, aug_topics_inv["Fork"]].astype(np.float64)
+# find the ratio of each signal to all signals at each time step
+for i in range(aug_data_smooth.shape[0]):
+	aug_data_smooth[i, :] = ah.safe_divide(aug_data_smooth[i, :], np.sum(aug_data_smooth[i, :]))
 
-
-aug_data_smooth = np.array(aug_data, np.float64)
+# smooth each signal
+w = 24 * 7
 for i_sig in range(aug_data_smooth.shape[1]):
-	aug_data_smooth[:, i_sig] = ah.causal_rolling_average(aug_data[:, i_sig], 24)
+	aug_data_smooth[:, i_sig] = ah.causal_rolling_average(aug_data_smooth[:, i_sig], w)
+
+# remove the warm-up period for the smoothing function
+aug_data_smooth[0:w, :] = np.ones((w, aug_data_smooth.shape[1]), dtype=np.float64) * aug_data_smooth[w, :]
+
+# normalise each signal
+for i_sig in range(aug_data_smooth.shape[1]):
 	aug_data_smooth[:, i_sig] = (aug_data_smooth[:, i_sig] - np.min(aug_data_smooth[:, i_sig])) / (np.max(aug_data_smooth[:, i_sig]) - np.min(aug_data_smooth[:, i_sig]))
 
-#aug_signal_a = aug_data_smooth[:, aug_topics_inv["Bullish"]]
-#aug_signal_a = aug_data_smooth[:, aug_topics_inv["Bearish"]]
-aug_signal_a = aug_data_smooth[:, aug_topics_inv["Institutional_money"]]
-
 # set up the figure
-fig, ax = plt.subplots(2, 1, sharex=True, sharey=False)
+fig, ax = plt.subplots(1, 1, sharex=True, sharey=False)
 
 # initialise some labels for the plot
 datenum_aug_data = [md.date2num(datetime.datetime.fromtimestamp(el)) for el in t_aug_data]
 datenum_price_data = [md.date2num(datetime.datetime.fromtimestamp(el)) for el in t_price_data]
 
 # plot stuff
-ax[0].grid(linewidth=0.4)
-ax[1].grid(linewidth=0.4)
-ax[0].plot(datenum_price_data, price_data, linewidth=0.5)
-#ax[1].plot(datenum_aug_data, aug_data.astype(np.float64), linewidth=0.5, alpha=0.5)
-ax[1].plot(datenum_aug_data, aug_data_smooth, linewidth=0.5, alpha=0.5)
-ax[1].plot(datenum_aug_data, aug_data_smooth[:, aug_topics_inv["Institutional_money"]], linewidth=0.5, color="b")
-ax[1].plot(datenum_aug_data, aug_data_smooth[:, aug_topics_inv["Fearful/Concerned"]], linewidth=0.5, color="r")
-#ax[1].plot(datenum_aug_data, aug_signal_b, linewidth=0.5, color="r")
+ax.grid(linewidth=0.4)
+ax.plot(datenum_price_data, price_data, linewidth=0.5,)
+ax2 = ax.twinx()
+ax2.plot(datenum_aug_data, aug_data_smooth[:, aug_topics_inv["Fork"]], linewidth=0.5, color="r", alpha=0.5)
 
 # label axes
-#ax[0].set_ylabel("__")
-#ax[1].set_ylabel("__")
-#ax[0].set_title("4_basic_strategy_example.py")
+ax.set_ylabel("Price (XBt)")
+ax2.set_ylabel("Fork")
 
 # generate the time axes
 plt.subplots_adjust(bottom=0.2)
 plt.xticks( rotation=25 )
-ax[0]=plt.gca()
+ax=plt.gca()
 xfmt = md.DateFormatter('%Y-%m-%d')
-ax[0].xaxis.set_major_formatter(xfmt)
+ax.xaxis.set_major_formatter(xfmt)
 
 # show the plot
 plt.show()
---
+++ src/data_loader_helper.py
@@ -0,0 +1,11 @@
+import datetime
+
+
+def load_data(price_symbol=None,
+              augmento_coin=None,
+              augmento_source=None,
+              datetime_start=None,
+              datetime_end=None):
+	
+	# check the input arguments
+	
