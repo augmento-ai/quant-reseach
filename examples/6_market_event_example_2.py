aa5e463c864c414283d48a28d1446b1aaaacd000
Oliver Spragg <oliver@converge.io>
2019-05-29 16:42:06 +0200

    set up market_event_example_2 to plot basic data from data_loader_helper

--- examples/6_market_event_example_2.py
+++ examples/6_market_event_example_2.py
@@ -29,70 +29,30 @@ all_data = dlh.load_data(path_data="data/cache",
               augmento_api_key=None)
 t_aug_data, t_bin_data, aug_data, bin_data, aug_keys, bin_keys = all_data
 
-pprint.pprint(aug_keys)
-pprint.pprint(bin_keys)
 
-
-quit()
-
-
-# define the location of the input file
-filename_augmento_topics = "data/example_data/augmento_topics.msgpack.zlib"
-filename_augmento_data = "data/example_data/augmento_data.msgpack.zlib"
-filename_bitmex_data = "data/example_data/bitmex_data.msgpack.zlib"
-
-# load the example data
-datetime_start = datetime.datetime(2018, 10, 8)
-datetime_end = datetime.datetime(2018, 12, 3)
-all_data = eh.load_example_data(filename_augmento_topics,
-                             filename_augmento_data,
-                             filename_bitmex_data,
-                             datetime_start=datetime_start,
-                             datetime_end=datetime_end)
-aug_topics, aug_topics_inv, t_aug_data, aug_data, t_price_data, price_data = all_data
-
-# initialise the smooth data
-aug_data_smooth = aug_data.astype(np.float64)
-
-# find the ratio of each signal to all signals at each time step
-for i in range(aug_data_smooth.shape[0]):
-	aug_data_smooth[i, :] = ah.safe_divide(aug_data_smooth[i, :], np.sum(aug_data_smooth[i, :]))
-
-# smooth each signal
-w = 24 * 7
-for i_sig in range(aug_data_smooth.shape[1]):
-	aug_data_smooth[:, i_sig] = ah.causal_rolling_average(aug_data_smooth[:, i_sig], w)
-
-# remove the warm-up period for the smoothing function
-aug_data_smooth[0:w, :] = np.ones((w, aug_data_smooth.shape[1]), dtype=np.float64) * aug_data_smooth[w, :]
-
-# normalise each signal
-for i_sig in range(aug_data_smooth.shape[1]):
-	aug_data_smooth[:, i_sig] = (aug_data_smooth[:, i_sig] - np.min(aug_data_smooth[:, i_sig])) / (np.max(aug_data_smooth[:, i_sig]) - np.min(aug_data_smooth[:, i_sig]))
 
 # set up the figure
-fig, ax = plt.subplots(1, 1, sharex=True, sharey=False)
+fig, ax = plt.subplots(2, 1, sharex=True, sharey=False)
 
 # initialise some labels for the plot
+datenum_bin_data = [md.date2num(datetime.datetime.fromtimestamp(el)) for el in t_bin_data]
 datenum_aug_data = [md.date2num(datetime.datetime.fromtimestamp(el)) for el in t_aug_data]
-datenum_price_data = [md.date2num(datetime.datetime.fromtimestamp(el)) for el in t_price_data]
 
 # plot stuff
-ax.grid(linewidth=0.4)
-ax.plot(datenum_price_data, price_data, linewidth=0.5,)
-ax2 = ax.twinx()
-ax2.plot(datenum_aug_data, aug_data_smooth[:, aug_topics_inv["Fork"]], linewidth=0.5, color="r", alpha=0.5)
-
-# label axes
-ax.set_ylabel("Price (XBt)")
-ax2.set_ylabel("Fork")
+ax[0].grid(linewidth=0.4)
+ax[1].grid(linewidth=0.4)
+ax[0].plot(datenum_bin_data, bin_data[:, bin_keys["close"]], linewidth=0.5)
+ax[1].plot(datenum_aug_data, aug_data, linewidth=0.5, alpha=0.5)
 
 # generate the time axes
 plt.subplots_adjust(bottom=0.2)
 plt.xticks( rotation=25 )
-ax=plt.gca()
-xfmt = md.DateFormatter('%Y-%m-%d')
-ax.xaxis.set_major_formatter(xfmt)
+ax[0]=plt.gca()
+xfmt = md.DateFormatter('%Y-%m-%d %H:%M')
+ax[0].xaxis.set_major_formatter(xfmt)
 
 # show the plot
 plt.show()
+
+
+
