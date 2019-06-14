b0aee0f4c357602413a64a755f66243bd7032fda
Oliver Spragg <oliver@converge.io>
2019-06-14 10:46:41 +0200

    added numba implementation of simple_backtest_a

--- src/analysis_helper.py
+++ src/analysis_helper.py
@@ -74,3 +74,26 @@ def nb_calc_sentiment_score_a(sent_a, sent_b, ra_win_size, std_win_size):
 	sent_ratio_smooth = nb_causal_rolling_average(sent_ratio, ra_win_size)
 	sent_score = nb_causal_rolling_sd(sent_ratio_smooth, std_win_size)
 	return sent_score
+
+@nb.jit("(f8[:])(f8[:], f8[:], f8)", nopython=True, nogil=True, cache=True)
+def nb_backtest_a(price, sent_score, buy_sell_fee):
+	pnl = np.zeros(price.shape, dtype=np.float64)
+	pnl[0] = 1.0
+	for i_p in range(1, price.shape[0]):
+		
+		# if sentiment score is positive, simulate long position
+		# else if sentiment score is negative, simulate short position
+		# else if the sentiment score is 0.0, hold
+		# (note that this is a very approximate market simulation!)
+		if sent_score[i_p-1] > 0.0:
+			pnl[i_p] = (price[i_p] / price[i_p-1]) * pnl[i_p-1]
+		elif sent_score[i_p-1] <= 0.0:
+			pnl[i_p] = (price[i_p-1] / price[i_p]) * pnl[i_p-1]
+		elif sent_score[i_p-1] == 0.0:
+			pnl[i_p] = pnl[i_p-1]
+		
+		# simulate a trade fee if we cross from long to short, or visa versa
+		if np.sign(sent_score[i_p]) != np.sign(sent_score[i_p-1]):
+			pnl[i_p] = pnl[i_p] - (buy_sell_fee * pnl[i_p])
+	
+	return pnl
