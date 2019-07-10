2f0203b184cf377658ca9ac738cb12b24605cb84
hoang777 <hoangluongeu@gmail.com>
2019-07-10 12:31:31 +0200

    add example 5

---
+++ examples/5_write_strategy_to_csv.py
@@ -0,0 +1,75 @@
+import sys
+import msgpack
+import zlib
+import numpy as np
+import pandas as pd
+import datetime
+import matplotlib.pyplot as plt
+import matplotlib.dates as md
+
+# import files from src
+sys.path.insert(0, "src")
+import example_helper as eh
+import analysis_helper as ah
+
+# define the location of the input file
+filename_augmento_topics = "data/example_data/augmento_topics.msgpack.zlib"
+filename_augmento_data = "data/example_data/augmento_data.msgpack.zlib"
+filename_bitmex_data = "data/example_data/bitmex_data.msgpack.zlib"
+
+# load the example data
+all_data = eh.load_example_data(filename_augmento_topics, filename_augmento_data, filename_bitmex_data)
+aug_topics, aug_topics_inv, t_aug_data, aug_data, t_price_data, price_data = all_data
+
+
+
+# get the signals we're interested in
+aug_signal_a = aug_data[:, aug_topics_inv["Positive"]].astype(np.float64)
+aug_signal_b = aug_data[:, aug_topics_inv["Bearish"]].astype(np.float64)
+
+sent_score = ah.nb_calc_sentiment_score_c(aug_signal_a, aug_signal_b, 28*24, 14*24)
+
+date_time = np.array([datetime.datetime.utcfromtimestamp(t).isoformat()
+ for t in t_price_data])
+
+# define some parameters for the backtest
+start_pnl = 1.0
+buy_sell_fee = 0.0075
+# run the backtest
+pnl = ah.nb_backtest_a(price_data, sent_score, start_pnl, buy_sell_fee)
+# set up the figure
+fig, ax = plt.subplots(3, 1, sharex=True, sharey=False)
+# initialise some labels for the plot
+datenum_aug_data = [md.date2num(datetime.datetime.fromtimestamp(el)) for el in t_aug_data]
+datenum_price_data = [md.date2num(datetime.datetime.fromtimestamp(el)) for el in t_price_data]
+# plot stuff
+ax[0].grid(linewidth=0.4)
+ax[1].grid(linewidth=0.4)
+ax[2].grid(linewidth=0.4)
+ax[0].plot(datenum_price_data, price_data, linewidth=0.5)
+ax[1].plot(datenum_aug_data, sent_score, linewidth=0.5)
+ax[2].plot(datenum_price_data, pnl, linewidth=0.5)
+# label axes
+ax[0].set_ylabel("Price")
+ax[1].set_ylabel("Seniment score")
+ax[2].set_ylabel("PnL")
+#ax[0].set_title("4_basic_strategy_example.py")
+# generate the time axes
+plt.subplots_adjust(bottom=0.2)
+plt.xticks( rotation=25 )
+ax[0]=plt.gca()
+xfmt = md.DateFormatter('%Y-%m-%d')
+ax[0].xaxis.set_major_formatter(xfmt)
+# show the plot
+plt.show()
+
+
+
+csv_data= {"date": date_time, "ref_price":price_data,"raw_signal":sent_score,"pnl":pnl}
+
+data_frame = pd.DataFrame(csv_data)
+
+data_frame.to_csv("data/signals.csv",index=False)
+
+
+
