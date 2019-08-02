14c143babc4eb2a2163a2d5300443c24e864f1b4
hoang777 <hoangluongeu@gmail.com>
2019-08-02 19:57:14 +0200

    add randomized windows

--- src/analysis_helper.py
+++ src/analysis_helper.py
@@ -60,11 +60,11 @@ def nb_causal_rolling_sd_rand(arr, window_size_rand, window_size_std):
 	# for each output element, find the mean and std of the last few
 	# input elements, and standardise the input element by the mean and std of the window
 	for i in nb.prange(out_arr.shape[0]):
-			window_size = round(np.random.normal(window_size_rand, window_size_std))       
-			num = new_arr[i+window_size-1] - np.mean(new_arr[i : i + window_size-1])
-			denom = np.std(new_arr[i : i + window_size-1])
-			if denom != 0.0:
-					out_arr[i] = num / denom
+		window_size = round(np.random.normal(window_size_rand, window_size_std))       
+		num = new_arr[i+window_size-1] - np.mean(new_arr[i : i + window_size-1])
+		denom = np.std(new_arr[i : i + window_size-1])
+		if denom != 0.0:
+			out_arr[i] = num / denom
 
 	return out_arr
 
@@ -87,25 +87,88 @@ def nb_causal_rolling_norm(arr, window_size):
 	
 	return out_arr
 	
-@nb.jit("(f8[:])(f8[:], i8, i8)", nopython=True, nogil=True, parallel=True)
-def nb_causal_rolling_norm_rand(arr, window_size_rand, window_size_std):
-	   
+@nb.jit("(f8[:])(f8[:], i8, f8)", nopython=True, nogil=True, parallel=True)
+def nb_causal_rolling_norm_rand(arr, window_size_rand, peturb):
 	# create an output array
 	out_arr = np.zeros(arr.shape[0])
 	
 	# create an array from the input array, with added space for the rolling window
 	new_arr = np.hstack((np.ones(window_size_rand-1) * arr[0], arr))
+
+	index_new = window_size_rand
 	
 	# for each output element, find the mean and std of the last few
 	# input elements, and standardise the input element by the mean and std of the window
 	for i in nb.prange(out_arr.shape[0]):
-			window_size = round(np.random.normal(window_size_rand, window_size_std))
-			num = new_arr[i+window_size-1] - np.mean(new_arr[i : i + window_size])
-			denom = np.max(np.abs(new_arr[i : i + window_size] - np.mean(new_arr[i : i + window_size])))
-			if denom != 0.0:
-					out_arr[i] = num / denom
+
+		window_size_std = peturb * np.float64(window_size_rand)
+		window_size = round(np.random.normal(window_size_rand, window_size_std))
+
+		i_end_new = i + window_size_rand
+		i_start_new = i_end_new - window_size
+
+		if i_start_new < 0:
+			i_start_new = 0
+
+		out_arr[i] = np.mean(new_arr[i_start_new : i_end_new])
+		#print(out_arr[i-1:i+1])
+
+		#num = new_arr[i+window_size-1] - np.mean(new_arr[i : i + window_size])
+		#denom = np.max(np.abs(new_arr[i : i + window_size] - np.mean(new_arr[i : i + window_size])))
+		#if denom != 0.0:
+		#	out_arr[i] = num / denom
 	
 	return out_arr
+
+
+@nb.jit("(f8[:])(f8[:], i8)", nopython=True, nogil=True, parallel=True)
+def nb_causal_rolling_average(arr, window_size):
+	
+	# create an output array
+	out_arr = np.zeros(arr.shape[0])
+	
+	# create an array from the input array, with added space for the rolling window
+	new_arr = np.hstack((np.ones(window_size-1) * arr[0], arr))
+	
+	# for each output element, find the mean of the last few input elements
+	for i in nb.prange(out_arr.shape[0]):
+		out_arr[i] = np.mean(new_arr[i : i + window_size])
+	
+	return out_arr
+
+
+
+#@nb.jit("(f8[:])(f8[:], f8[:], i8, i8, f8)", nopython=True, nogil=True)
+def nb_calc_sentiment_score_rand_b(sent_a, sent_b, ra_win_size_short, ra_win_size_long,peturb):
+	# example method for creating a stationary sentiment score based on Augmento data
+	
+	# compare the raw sentiment values
+	sent_ratio = nb_safe_divide(sent_a, sent_b)
+	
+	# smooth the sentiment ratio
+	sent_ratio_short = nb_causal_rolling_norm_rand(sent_ratio, ra_win_size_short, peturb)
+	sent_ratio_long = nb_causal_rolling_norm_rand(sent_ratio, ra_win_size_long, peturb)
+	
+	# create a stationary(ish) representation of the smoothed sentiment ratio
+	sent_score = sent_ratio_short - sent_ratio_long
+	
+	return sent_score
+
+
+@nb.jit("(f8[:])(f8[:], f8[:], i8, i8, f8)", nopython=True, nogil=True)
+def nb_calc_sentiment_score_rand_a(sent_a, sent_b, ra_win_size, std_win_size, peturb):
+	# example method for creating a stationary sentiment score based on Augmento data
+	
+	# compare the raw sentiment values
+	sent_ratio = nb_safe_divide(sent_a, sent_b)
+	
+	# smooth the sentiment ratio
+	sent_ratio_smooth = nb_causal_rolling_norm_rand(sent_ratio, ra_win_size, peturb)
+	
+	# create a stationary(ish) representation of the smoothed sentiment ratio
+	sent_score = nb_causal_rolling_sd(sent_ratio_smooth, std_win_size)
+	
+	return sent_score
 
 @nb.jit("(f8[:])(f8[:], f8[:], i8, i8)", nopython=True, nogil=True)
 def nb_calc_sentiment_score_a(sent_a, sent_b, ra_win_size, std_win_size):
@@ -186,19 +249,19 @@ def nb_backtest_a(price, sent_score, start_pnl, buy_sell_fee):
 
 @nb.jit("(f8[:])(f8[:], i8)", nopython=True, nogil=True, cache=True)
 def moving_average(arr, window):
-        
-        # output array
+		
+		# output array
 	ma_arr = np.zeros(arr.shape[0])
-        
-        # add space for rolling window
+		
+		# add space for rolling window
 	new_arr = np.hstack((np.ones(window-1) * arr[0], arr))
-        
-        # calculate moving average
+		
+		# calculate moving average
 	for i in nb.prange(arr.shape[0]):
 		num = new_arr[i+window-1] - np.mean(new_arr[i : i+window-1])
 		denom = np.std(new_arr[i : i + window-1])
 		if denom != 0.0:
-                        ma_arr[i] = num / denom
+						ma_arr[i] = num / denom
 
 	return ma_arr 
 
